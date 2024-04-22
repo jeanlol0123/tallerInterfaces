1076ab3078b88840ca374fb0423ffa8675e7ed6e
Disruptor <davidsantiagojimenez16@gmail.com>
2024-04-21 20:13:29 -0500

    Tarea

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
-
+import { Celular } from './producto.interface';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  arrCells:Celular[] = [];
 
-  constructor() {}
+  formData:FormGroup;
+
+  ngOnInit(){
+    this.formData = new FormGroup({
+      marca: new FormControl(),
+      modelo: new FormControl(),
+      precio: new FormControl(),
+      almacenamiento: new FormControl()
+    })
+  }
+
+
+  agregarTelefono(){
+      let celular: Celular = {
+        marca: this.formData.get('marca')?.value,
+        modelo: this.formData.get('modelo')?.value,
+        precio: this.formData.get('precio')?.value,
+        almacenamientoInterno: this.formData.get('almacenamiento')?.value
+      };
+
+      this.arrCells.push(celular);
+      this.formData.reset();
+  }
+
+  mostrarArrays(){
+    console.log(this.arrCells);
+  }
+
+  
 
 }
---
+++ src/app/home/producto.interface.ts
@@ -0,0 +1,6 @@
+export interface Celular {
+    marca: string;
+    modelo: string;
+    precio: number;
+    almacenamientoInterno: number;
+  }
